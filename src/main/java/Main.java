7475b35aff7aee9ad60e8ca7ab2d991cbf9688a4
liuxinyu <1835583124@qq.com>
2020-05-30 21:39:18 +0800

    编译JDK8

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName : Main
+ * @Description : Main主类
+ * @Author : LiuXinyu
+ * @Site : www.imlxy.cn
+ * @Date: 2020-03-26 21:45
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/test/hashmap/Test.java
@@ -0,0 +1,22 @@
+package test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName : Test
+ * @Description :
+ * @Author : LiuXinyu
+ * @Site : www.imlxy.cn
+ * @Date: 2020-03-27 10:52
+ */
+public class Test {
+    public static void main(String[] args) {
+        Map<String, Double> map=new HashMap<>();
+        map.put("k1", 0.1);
+        map.put("k2", 0.2);
+        map.put("k3", 0.3);
+        map.put("k4", 0.4);
+        map.put("k5", 0.5);
+    }
+}
